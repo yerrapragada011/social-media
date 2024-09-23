15743a7288855d84f45f6c8003b6fe8891c3ef51
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-09-23 14:12:22 -0400

    Add padding for posts

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Post({ post }) {
   return (
-    <div style={{ border: '1px solid gray' }}>
+    <div style={{ border: '1px solid gray', padding: '20px' }}>
       <h2>{post.title}</h2>
       <p>Author: {post.author?.username}</p>
       <Link to={`/posts/${post.id}`}>View Details</Link>
